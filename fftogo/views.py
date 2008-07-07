60a7615f1216abe0b100883f69b6fb01c258d8a2
Benjamin Golub <bgolub@benjamingolub.com>
2008-07-07 16:35:18 -0400

    FriendFeed modified their HTTP response code system; now if you try to access something you don't have access to it will 403 instead of 401ing.  A 401 strictly means wrong authorization now, nice :)

--- fftogo/views.py
+++ fftogo/views.py
@@ -444,6 +444,9 @@ def room(request, nickname):
         data = f.fetch_room_feed(nickname, num=num, start=start,
             service=service)
     except Exception, e:
+        if e[0] == 401:
+            del request.session['nickname']
+            del request.session['key']
         return HttpResponseRedirect(reverse(str(e)))
     try:
         profile = f.fetch_room_profile(nickname)
@@ -637,6 +640,9 @@ def user(request, nickname, type=None):
                 service=service)
         profile = f.fetch_user_profile(nickname)
     except Exception, e:
+        if e[0] == 401:
+            del request.session['nickname']
+            del request.session['key']
         return HttpResponseRedirect(reverse(str(e)))
     entries = [entry for entry in data['entries'] if not entry['hidden']]
     hidden = [entry for entry in data['entries'] if entry['hidden']]
