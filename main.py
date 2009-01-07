4b6fcbe41ab92468b4cad92a45ccc2b63f3dc18a
Benjamin Golub <bgolub@benjamingolub.com>
2009-01-07 11:00:07 -0800

    use zipimport

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os, sys
-
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
+import os
+import sys
 
 # Remove the standard version of Django.
 for k in [k for k in sys.modules if k.startswith('django')]:
@@ -11,21 +9,26 @@ for k in [k for k in sys.modules if k.startswith('django')]:
 # from it.
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Must set this env var *before* importing any part of Django
+# Import Django from a zipfile.
+sys.path.insert(0, os.path.abspath('django.zip'))
+
+# Google App Engine imports.
+from google.appengine.ext.webapp import util
+
+# Custom Django configuration.
+from django.conf import settings
+settings._target = None
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 # Import the part of Django we need.
 import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
 
 def main():
-  # Create a Django application for WSGI.
-  application = django.core.handlers.wsgi.WSGIHandler()
+    # Create a Django application for WSGI.
+    application = django.core.handlers.wsgi.WSGIHandler()
+    # Run the WSGI CGI handler with that application.
+    util.run_wsgi_app(application)
 
-  # Run the WSGI CGI handler with that application.
-  util.run_wsgi_app(application)
 
 if __name__ == '__main__':
-  main()
+    main()
